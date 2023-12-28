8e680de29b783d0afab1e941a2e8879eb724eae4
ismailyazan01 <ismailyazan01@gmail.com>
2023-12-27 19:23:57 -0800

    Added a MySQL DB to track completed and attempted recurring tasks. Changed method naming. Added urgency, "Unaccounted events", end of day event entry features, and documentation.

--- notifications.py
+++ notifications.py
@@ -4,16 +4,19 @@ import time
 from email.message import EmailMessage
 from dotenv import load_dotenv
 from datetime import datetime
+import mysql.connector
+
 
 # Load environment variables from a .env file
 load_dotenv()
 
 # Initialize an empty list to store the schedule
 schedule = []
+incompleteToDo = []
 
 
 # Function to read the schedule from a text file
-def read_schedule(txtfile):
+def readSchedule(txtfile):
     """
     Reads a schedule from a text file and stores it in the global schedule list.
     Each line in the file is expected to be either a header or an event.
@@ -31,12 +34,12 @@ def read_schedule(txtfile):
             else:
                 # Otherwise, split the line at ' @ ' and process the time
                 rendezvous = line.split(' @ ')
-                rendezvous[1] = read_time(rendezvous[1])
+                rendezvous[1] = readTime(rendezvous[1])
                 schedule.append([rendezvous[0], rendezvous[1]])
 
 
 # Function to send an email alert
-def email_alert(subject, body, to):
+def emailAlert(subject, body, to):
     """
     Sends an email alert.
 
@@ -65,7 +68,7 @@ def email_alert(subject, body, to):
 
 
 # Function to process and format time strings
-def read_time(time):
+def readTime(time):
     """
     Converts time strings into a 24-hour format.
 
@@ -106,12 +109,12 @@ def read_time(time):
 
 
 # Function to run the notification process
-def run_notifications():
+def runNotifications():
     """
     Runs the notification process by reading the schedule and sending emails
     at the specified times.
     """
-    read_schedule("schedule.txt")  # Read the schedule from 'schedule.txt'
+    readSchedule("schedule.txt")  # Read the schedule from 'schedule.txt'
 
     for event in schedule[1:]:
         current_time = datetime.now().strftime("%H:%M:%S")  # Get current time
@@ -124,12 +127,123 @@ def run_notifications():
         # Calculate time difference
         time_diff = (target_datetime - current_datetime).total_seconds()
 
+        if time_diff < 0:
+            continue
+
+        if "urgent" in event[0].lower():
+            time.sleep(time_diff - 300.0)
+            emailAlert(event[1], event[0], os.getenv("TO_EMAIL"))
+            time_diff = 300
+
         # Wait until the target time and send an email alert
-        print(f"Waiting for {time_diff} seconds until {target_time}")
+        print(f"Waiting for {time_diff // 60} minutes until {target_time} for {event[0]}")
         time.sleep(time_diff)
-        email_alert(event[1], event[0], os.getenv("TO_NUMBER"))
-        email_alert(event[1], event[0], os.getenv("TO_EMAIL"))
+        emailAlert(event[1], event[0], os.getenv("TO_EMAIL"))
+
+        if "urgent" in event[0].lower():
+            time.sleep(300.0)
+            emailAlert(event[1], event[0], os.getenv("TO_EMAIL"))
+    # Handle unaccounted events and database connection at the end of the day
+    unaccountedEventsMethod()
+    connectToDatabase()
+    endDayEventEntry()
+
+
+def unaccountedEventsMethod():
+    """
+        Interactively adds unaccounted events to the schedule. It prompts the user to enter
+        events and specify if they are recurring. The process continues until the user types 'done'.
+        """
+    # Prompt user for unaccounted events until they signal completion
+    unaccountedEvent = ""
+    while unaccountedEvent.lower() != "done":
+        unaccountedEvent = input("Add unaccounted for events(Type \"done\" to quit): ")
+        if unaccountedEvent.lower() == "done":
+            return
+        recurring = input("Add recurring(Y or N)? ")
+        if recurring.lower() == 'y':
+            schedule.append("(recurring)" + unaccountedEvent)
+        else:
+            schedule.append(unaccountedEvent)
+
+
+def endDayEventEntry():
+    """
+        Processes each event at the end of the day, asking the user if they completed it.
+        Based on the user's response, it updates the event's status as complete or incomplete.
+        """
+    # Iterate through each event and update its completion status
+    for event in schedule[1:]:
+        if type(event) == list:
+            curEvent = event[0]
+        else:
+            curEvent = event
+        if input(f"Did you complete this event: \"{curEvent}\"(Y or N)? ").lower() == 'n':
+            recurringEventHelper(curEvent, 'N')
+        else:
+            recurringEventHelper(curEvent, 'Y')
+
+
+def connectToDatabase():
+    """
+        Establishes a connection to the database using credentials from environment variables.
+
+        Returns:
+        Connection object: The connection to the database.
+        """
+    # Connect to the MySQL database with credentials from environment variables
+    db_connection = mysql.connector.connect(
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME')
+    )
+    return db_connection
+
+
+def recurringEventHelper(event, complete):
+    """
+    Updates the event's completion and planned status in the database.
+    It determines the event's category and constructs the appropriate SQL query to update the database.
+
+    Parameters:
+    event (str): The name of the event to update.
+    complete (str): 'Y' if the event was completed, 'N' otherwise.
+    """
+    # Determine the event's category and construct the SQL query
+    identifier = ""
+    eventLowerCase = event.lower()
+
+    keywords = {
+        "Prayer": ["fajir", "duhr", "asr", "maghrib", "isha", "jumuah"],
+        "Reading": ["read", "reading"],
+        "Workout": ["workout", "exercise", "football", "basketball", "boeing", "soccer"],
+        "Quran": ["quran", "halaqa"],
+        "Coding": ["coding", "program"],
+        "School": ["school", "class"]
+    }
+
+    for category, words in keywords.items():
+        if any(word in eventLowerCase for word in words):
+            identifier = category
+            break
+
+    # Execute the SQL query to update the event's status
+    connection = connectToDatabase()
+    cursor = connection.cursor()
+
+    if "(recurring)" not in eventLowerCase:
+        incompleteToDo.append(event)
+
+    if complete.lower() == 'y':
+        update_query = "UPDATE events SET completed = completed + 1, planned = planned + 1 WHERE event_name = %s"
+    else:
+        update_query = "UPDATE events SET planned = planned + 1 WHERE event_name = %s"
+    cursor.execute(update_query, (identifier, ))
+    connection.commit()
+    cursor.close()
+    connection.close()
 
 
 # Run the notification process
-run_notifications()
+runNotifications()
